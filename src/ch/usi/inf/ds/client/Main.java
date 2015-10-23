96dcbea46c4a6f238dd2b5c0ab78452f9c685fde
Samantha Rosso <sammyrosso@hotmail.com>
2015-10-23 19:15:07 +0200

    Sample use of Watcher

--- src/ch/usi/inf/ds/client/Main.java
+++ src/ch/usi/inf/ds/client/Main.java
@@ -1,8 +1,20 @@
 package ch.usi.inf.ds.client;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(final String[] args) throws IOException {
+		final Watcher watcher = new Watcher();
+		final Path path = Paths.get("test_dir");
+		System.out.println(path.toAbsolutePath());
+		try {
+			watcher.watch(path);
+		} catch (final InterruptedException e) {
+			System.out.println("Bye");
+		}
+
+	}
 }
