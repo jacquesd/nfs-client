c9eff4efe63d5b67435d781f4329d6ea942c32f5
Jacques Dafflon <jacques.dafflon@gmail.com>
2015-12-10 17:19:47 +0100

    Cleans up FileUtil

--- src/ch/usi/inf/ds/nfsclient/files/FileUtil.java
+++ src/ch/usi/inf/ds/nfsclient/files/FileUtil.java
@@ -5,13 +5,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
-public class FileUtil {
+public final class FileUtil {
+    private FileUtil() {}
+
     public static Stream<Path> forSubDirectory(final Path dir) throws IOException {
-        return Files.walk(dir)
-                .filter(Files::isDirectory);
+        return Files.walk(dir).filter(Files::isDirectory);
     }
 
-    public static Stream<Path> walk(final Path dir) throws IOException {
-        return Files.walk(dir);
-    }
+    public static Stream<Path> walk(final Path dir) throws IOException { return Files.walk(dir); }
 }
